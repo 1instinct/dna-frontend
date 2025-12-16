3868a0e04221763e09d758a9a3830acaf52cab36
Aaron Smulktis <aaronsmulktis@gmail.com>
2025-12-16 13:57:56 -0800

    linter fixes

--- components/shared/Logo/index.ts
+++ components/shared/Logo/index.ts
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 export { Logo } from "./Logo";
 =======
 export { Logo } from "./Logo";
 >>>>>>> 5e30af0 (get better svg logo, tweak questions, add ThreeViewer)
+=======
+export { Logo } from "./Logo";
+>>>>>>> ba1fd07 (linter fixes)
